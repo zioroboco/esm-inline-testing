47633eceb4eb37edac814a227b72b009f0a322ba
Louis Quinnell <_@ziorobo.co>
2022-04-02 14:00:51 +1100

    Add uvu and some inline tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 export const bee = "🐝"
+
+import { test } from "uvu"
+import * as assert from "uvu/assert"
+
+test(`bee`, () => {
+  assert.equal(bee, "🐝")
+})
+
+test("Math.sqrt", () => {
+  assert.is(Math.sqrt(4), 2)
+  assert.is(Math.sqrt(144), 12)
+  assert.is(Math.sqrt(2), Math.SQRT2)
+})
+
+test.run()
